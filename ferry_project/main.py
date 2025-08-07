0f6887655d39cdea32c84ec53ee143ea07ff8506
furkanavci10 <furkan_avci10@hotmail.com>
2025-08-07 17:31:52 +0300

    araç ekleme işlemi tamam silme işleminde kaldım

--- ferry_project/main.py
+++ ferry_project/main.py
@@ -1,14 +1,46 @@
-#car = 1
-#vehicle_1 = input("Lütefen araç türünüzü giriniz.")
-#if vehicle_1 == 1:
-#    print("Araç türünüz otomobildir")
-#else:
-#    pass    
-#vehicle_2 = input
+def add_vehicle():
+    vehicle_type = input("Araç bilgisi gir:")
+    car_dct = {}
+    motorcycle_dct = {}
+    truck_dct = {}
+    vehicle_all = {}
+    vehicle_all.update(car_dct)
+    vehicle_all.update(motorcycle_dct)
+    vehicle_all.update(truck_dct)
+    if (vehicle_type.lower() == "car"):
+        car_brand = input(str("Arabanın markası:"))
+        car_number_plate = input(str("Arabanızın plakasını giriniz:"))
+        car_dct["vehicle_type"] = vehicle_type
+        car_dct["car_brand"] = car_brand
+        car_dct["car_number_plate"] = car_number_plate
+        print(car_dct)
+    elif (vehicle_type.lower() == "motorcycle"):
+        motorcycle_brand = input(str("Motorsikletinizin markası:"))
+        motorcycle_number_plate = input(str("Motorsikletinizin plakasını giriniz"))
+        motorcycle_dct["vehicle_type"] = vehicle_type
+        motorcycle_dct["motorcycle_brand"] = motorcycle_brand
+        motorcycle_dct["motorcycle_number_plate"] = motorcycle_number_plate
+        print(motorcycle_dct)
+    elif (vehicle_type.lower() == "truck"):
+        truck_brand = input(str("Kamyonuzun markası:"))
+        truck_number_plate = input(str("Kamyonuzun plakasını giriniz"))
+        truck_dct["vehicle_type"] = vehicle_type
+        truck_dct["truck_brand"] = truck_brand
+        truck_dct["truck_number_plate"] = truck_number_plate
+        print(truck_dct)
+    else:
+        print("Geçersiz araç!")
+    print("Mevcut kayıtlar", vehicle_all)
+add_vehicle()
 
-vehicle = input("Araç bilgisi gir")
 
-if (vehicle == "Otomobil"):
-    print("Otomobil")
-elif (vehicle == "Motor"):
-    print("Motor")
+def del_vehicle():
+    vehicle_all = {}
+    number_plate = input("Silmek istediğiniz araç plakası")
+    if number_plate in vehicle_all:
+        silinen = vehicle_all.pop(number_plate)  
+        print(f"{number_plate}:{silinen},silindi.")
+del_vehicle()
+      
+    
+     
--- ferry_project/models/vehicle.py
+++ ferry_project/models/vehicle.py
@@ -1,21 +1,19 @@
 class Vehicle:
-    def __init__(self, brand, model, year, vehicle_type):
+    def __init__(self, brand, vehicle_type):
         self.brand = brand
-        self.model = model
-        self.year = year
         self.vehicle_type = vehicle_type
 
 class Car(Vehicle):
-    def __init__(self, brand, model, year, vehicle_type, number_plate):
+    def __init__(self, brand, vehicle_type, number_plate):
         self.number_plate = number_plate
-        super().__init__(brand, model, year, vehicle_type)
+        super().__init__(brand, vehicle_type)
 
 class Truck(Vehicle):
-    def __init__(self, brand, model, year, vehicle_type, number_plate):
+    def __init__(self, brand, vehicle_type, number_plate):
         self.number_plate = number_plate
-        super().__init__(brand, model, year, vehicle_type)        
+        super().__init__(brand, vehicle_type)        
         
 class Motorcycle(Vehicle):
-    def __init__(self, brand, model, year, vehicle_type, number_plate):
+    def __init__(self, brand, vehicle_type, number_plate):
         self.number_plate = number_plate
-        super().__init__(brand, model, year, vehicle_type)
+        super().__init__(brand, vehicle_type)
